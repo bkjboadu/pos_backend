f1c5822dc510f1136c4ec484f71e11f46124670b
bright <brbojr@gmail.com>
2025-01-06 08:52:55 -0700

    deploy-to-heroku.sh updated

--- deploy-to-heroku.sh
+++ deploy-to-heroku.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "checking out to the main branch"
-git checkout main
+git checkout master
 
 echo "Resetting local main branch to match origin/main"
-git reset --hard origin/main
+git reset --hard origin/master
 
 echo "Force pushing to heroku main ..."
-git push heroku main --force
+git push heroku master --force
 
 echo "deployment to heroku completed successfully"
