077d4d4df41eb43879dc3cde5648b083ff3a5406
bright <brbojr@gmail.com>
2025-01-09 16:14:56 -0700

    customer updated

--- customers/admin.py
+++ customers/admin.py
@@ -3,4 +3,8 @@ from .models import Customer
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone_number', 'created_at', 'updated_at', 'created_by',)
+    list_display = (
+        'first_name', 'last_name',
+        'email', 'phone_number',
+        'created_at', 'updated_at', 'created_by',
+    )
